7805996e696b12b43680bae1a731b37b600d271c
[Sirom] <[sirom2010@gmail.com]>
2017-10-27 17:41:47 +0700

    Index Register Login Cordova

---
+++ www/js/index.js
@@ -0,0 +1,9 @@
+function toIndex() {
+    window.location = "index.html";
+}
+function toRegis() {
+    window.location = "register.html";
+}
+function toLogin() {
+    window.location = "login.html";
+}
